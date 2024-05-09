22240e78901778aa6ce2011ed7fbafdfc58b26fe
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-05-10 03:34:47 +0600

    first commit

---
+++ class_object.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        print("A new object created")
+
+S1 = Student("Miju", "027")
+S2 = Student("Riju", "028")
+
